650d477c80ffe171e89a628dbeaf970024bb60a5
Samuel <samuguemar@gmail.com>
2022-11-19 09:18:32 +0000

    feat: add simple password generator

---
+++ src/components/sguemar/PasswordGenerator.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const letters = 'abcdefghijklmnñopqrstuvwyz'
+
+const PasswordGenerator = () => {
+	const [passLength, setPassLength] = useState(8)
+	const [password, setPassword] = useState('')
+
+	const handleChangeLength = (e) => {
+		const newLength = e.target.value
+		setPassLength(newLength)
+		generateNewPassword()
+	}
+
+	const generateNewPassword = () => {
+		let newPassword = ''
+		for (let i = 0; i < passLength; i++) {
+			const position = Math.floor(Math.random() * letters.length)
+			const newChar = letters.charAt(position)
+			newPassword = `${newPassword}${newChar}`
+		}
+		setPassword(newPassword)
+	}
+
+	return (
+		<div className='flex flex-col gap-5'>
+			<section className='border rounded h-14'>
+				<p className='h-full p-4 flex items-center justify-center text-lg'>{password}</p>
+			</section>
+			<p>Length {passLength}</p>
+			<input
+				className='h-1 rounded bg-indigo-700 appearance-none'
+				max={16}
+				min={8}
+				step={1}
+				name='password-length'
+				onChange={handleChangeLength}
+				type='range'
+				value={passLength}
+			/>
+			<button
+				className='w-full box-border border border-indigo-700 rounded p-4'
+				type='button'
+				onClick={generateNewPassword}
+			>
+				Generate password
+			</button>
+		</div>
+	)
+}
+
+export default PasswordGenerator
