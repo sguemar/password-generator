772d5a3592bde99300df590ec52e93c40c94584d
Samuel <samuguemar@gmail.com>
2023-01-28 21:55:43 +0000

    feat: add icon to copy password to clipboard

--- src/components/sguemar/PasswordGenerator.jsx
+++ src/components/sguemar/PasswordGenerator.jsx
@@ -24,10 +24,28 @@ const PasswordGenerator = () => {
 		setPassword(newPassword)
 	}
 
+	const copyToClipboard = () => {
+		navigator.clipboard.writeText(password).then(
+			() => {
+				console.log('clipboard successfully set')
+			},
+			() => {
+				throw new Error('clipboard write failed')
+			}
+		)
+	}
+
 	return (
 		<div className='flex flex-col gap-5'>
-			<section className='border rounded h-14'>
-				<p className='h-full p-4 flex items-center justify-center text-lg'>{password}</p>
+			<section className='flex gap-2'>
+				<div className='grow border rounded h-14'>
+					<p className='h-full p-4 flex items-center justify-center text-lg'>{password}</p>
+				</div>
+				<div className='flex-none self-center cursor-pointer' onClick={copyToClipboard}>
+					<svg fill='white' xmlns='http://www.w3.org/2000/svg' height='48' width='48'>
+						<path d='M9 43.95q-1.2 0-2.1-.9-.9-.9-.9-2.1V10.8h3v30.15h23.7v3Zm6-6q-1.2 0-2.1-.9-.9-.9-.9-2.1v-28q0-1.2.9-2.1.9-.9 2.1-.9h22q1.2 0 2.1.9.9.9.9 2.1v28q0 1.2-.9 2.1-.9.9-2.1.9Zm0-3h22v-28H15v28Zm0 0v-28 28Z' />
+					</svg>
+				</div>
 			</section>
 			<p>Length {passLength}</p>
 			<input
